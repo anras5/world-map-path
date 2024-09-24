3ca033a694722da70ef818bd8813e1deac72f512
anras5 <anras1filip@gmail.com>
2024-09-24 15:28:27 +0200

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "globe.svg",
+        "apple-touch-icon-180x180.png",
+        "maskable-icon-512x512.png",
+      ],
+      manifest: {
+        name: "World Map Path",
+        short_name: "WorldMap",
+        description:
+          "World Map Path allows you to calculate distance of a path",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/world-map-path/", // Replace with your repository name
 });
