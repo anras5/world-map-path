eee1ed4f9611eb366cbb80be01844ca804f417f8
anras5 <anras1filip@gmail.com>
2025-09-27 13:30:46 +0200

    smaller icon sizes

--- src/App.tsx
+++ src/App.tsx
@@ -31,10 +31,10 @@ const customIcon = new L.Icon({
   iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
-  iconSize: [25, 41], // size of the icon
-  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  iconSize: [15, 26], // size of the icon
+  iconAnchor: [8, 26], // point of the icon which will correspond to marker's location
   popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
-  shadowSize: [41, 41], // size of the shadow
+  shadowSize: [26, 26], // size of the shadow
 });
 
 interface MapWithPathProps {
